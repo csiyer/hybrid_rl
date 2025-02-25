d58d0543430806e498e39289bc495b554eef74f1
Chris Iyer <chris.s.iyer@gmail.com>
2025-02-25 14:29:06 -0500

    check glms

--- fmri_analysis/scripts/transfer_data_to_ginsburg.sh
+++ fmri_analysis/scripts/transfer_data_to_ginsburg.sh
@@ -1,4 +1,4 @@
-rsync -zhave --progress \
+rsync -zhave --progress --ignore-existing \
     --exclude='TCST001' --exclude='TCST003' --exclude='TCST011' --exclude='TCST035' --exclude='TCST035_2nd' \
     --exclude='behavior/01_output/*' --exclude='behavior/03_output/*' \
     --exclude='behavior/11_output/*' --exclude='behavior/35_output/*' --exclude='behavior/35_2nd_output/*' \
@@ -15,7 +15,7 @@ rsync -zhave --progress \
     --include='TCST*/hybrid_r?/preproc_6mm_6del_100s_mc.feat/mc/extended_confs_24par.txt' \
     --exclude='*'
 
-rsync -zhave -- progress \
+rsync -zhave --progress --ignore-existing \
     /Users/chrisiyer/_Current/lab/code/hybrid_rl/fmri_analysis/scripts \
     csi2108@ginsburg.rcs.columbia.edu:/burg/dslab/users/csi2108/scripts
 
